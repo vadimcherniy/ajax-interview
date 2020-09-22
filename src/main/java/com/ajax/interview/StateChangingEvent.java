e32d7766401a4ffab07d9cc881234d905844253d
surovskyi <surovskyi.y@ajax.systems>
2020-09-22 12:28:11 +0300

    initial

--- src/main/java/com/ajax/interview/StateChangingEvent.java
+++
@@ -1,9 +0,0 @@
-package com.ajax.interview;
-
-import lombok.Data;
-
-@Data
-public class StateChangingEvent {
-    private String deviceId;
-    private State state;
-}
--- src/main/java/com/ajax/interview/TemperatureChangingEvent.java
+++
@@ -1,9 +0,0 @@
-package com.ajax.interview;
-
-import lombok.Data;
-
-@Data
-public class TemperatureChangingEvent {
-    private String deviceId;
-    private int temperatureDiff;
-}
