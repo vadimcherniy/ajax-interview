80f344148a8b7c1af1fefe34b6c789e81b7d14a1
vadym.chornyy <vchornyi@intelliarts.com>
2022-02-11 21:17:43 +0200

    service impl

---
+++ src/main/java/com/ajax/interview/exceptions/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.ajax.interview.exceptions;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/test/java/com/ajax/interview/service/FireProtectServiceTest.java
@@ -0,0 +1,10 @@
+package com.ajax.interview.service;
+
+import junit.framework.TestCase;
+
+public class FireProtectServiceTest extends TestCase {
+    private final FireProtectService fireProtectService = new FireProtectServiceImpl();
+
+    public void testChangeState() {
+    }
+}
